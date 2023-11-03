9f716f313970c363356bb2ad5bdc825d59c0a921
Mike Sheehy <sheehy@outlook.com>
2023-11-03 05:04:57 -0400

    add details component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-import { RouterModule} from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -11,7 +11,7 @@ import { RouterModule} from '@angular/router';
         <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
       </header>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
 
     </main>
