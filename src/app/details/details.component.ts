4275543e8bfd8685ec63ac505fbbe987a29dd72f
Mike Sheehy <sheehy@outlook.com>
2023-11-08 22:59:26 -0500

    Submit application values to console

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -26,7 +26,7 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
     </section>
     <section class="listing-apply">
       <h2 class="section-heading">Apply now to live here</h2>
-      <form [formGroup]="applyForm">
+      <form [formGroup]="applyForm" (submit)="submitApplication()">
         <label for="first-name">First Name</label>
         <input id="first-name" type="text" formControlName="firstName">
 
@@ -56,5 +56,12 @@ export class DetailsComponent {
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id'])
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    )
   }
 }
