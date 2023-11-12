51ebc7beebc674e7a6fa164759f56870a86c3c68
Mike Sheehy <sheehy@outlook.com>
2023-11-12 17:21:10 -0500

    Use fetch to create HTTP requests

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -9,12 +9,14 @@ export class HousingService {
 
   constructor() { }
 
-  getAllHousingLocations() : HousingLocation[] {
-    return this.housingLocationList;
+  async getAllHousingLocations() : Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
   }
 
-  getHousingLocationById(id: Number): HousingLocation | undefined{
-    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  async getHousingLocationById(id: Number): Promise<HousingLocation | undefined>{
+    const data = await fetch(`${this.url}/${id}`);
+    return await data.json() ?? {};
   }
   submitApplication(firstName: string, lastName: string, email: string) {
     console.log(firstName, lastName, email);
